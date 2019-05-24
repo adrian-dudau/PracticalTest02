77fd7c4a447ab9b421cb08e761edf998b5b83a39
Root USO User <student@stud.acs.upb.ro>
2019-05-24 11:04:03 +0300

    am trecut materia(sper)

---
+++ PracticalTest02/app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/general/Constants.java
@@ -0,0 +1,19 @@
+package ro.pub.cs.systems.eim.practicaltest02.general;
+
+public interface Constants {
+
+    final public static String TAG = "[PracticalTest02]";
+
+    final public static boolean DEBUG = true;
+
+    final public static String WEB_SERVICE_ADDRESS1 = "http://help.websiteos.com/websiteos/example_of_a_simple_html_page.htm";
+    final public static String WEB_SERVICE_ADDRESS2 = "http://help.websiteos.com/websiteos/example_of_a_simple_html_page.htm";
+
+    final public static String EMPTY_STRING = "";
+
+    final public static String SCRIPT_TAG = "script";
+    final public static String SEARCH_KEY = "wui.api_data =\n";
+
+    final public static String CURRENT_OBSERVATION = "current_observation";
+
+}
---
+++ PracticalTest02/app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/network/CommunicationThread.java
@@ -0,0 +1,118 @@
+package ro.pub.cs.systems.eim.practicaltest02.network;
+
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.Socket;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import cz.msebera.android.httpclient.NameValuePair;
+import cz.msebera.android.httpclient.client.HttpClient;
+import cz.msebera.android.httpclient.client.ResponseHandler;
+import cz.msebera.android.httpclient.client.entity.UrlEncodedFormEntity;
+import cz.msebera.android.httpclient.client.methods.HttpPost;
+import cz.msebera.android.httpclient.impl.client.BasicResponseHandler;
+import cz.msebera.android.httpclient.impl.client.DefaultHttpClient;
+import cz.msebera.android.httpclient.message.BasicNameValuePair;
+import cz.msebera.android.httpclient.protocol.HTTP;
+import ro.pub.cs.systems.eim.practicaltest02.general.Constants;
+import ro.pub.cs.systems.eim.practicaltest02.general.Utilities;
+import ro.pub.cs.systems.eim.practicaltest02.model.WeatherForecastInformation;
+
+public class CommunicationThread extends Thread {
+
+    private ServerThread serverThread;
+    private Socket socket;
+
+    private String allowedURL = Constants.WEB_SERVICE_ADDRESS1;
+
+    public CommunicationThread(ServerThread serverThread, Socket socket) {
+        this.serverThread = serverThread;
+        this.socket = socket;
+    }
+
+    public static String getHTML(String urlToRead) throws Exception {
+        StringBuilder result = new StringBuilder();
+        URL url = new URL(urlToRead);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String line;
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
+        }
+        rd.close();
+        return result.toString();
+    }
+
+    @Override
+    public void run() {
+        if (socket == null) {
+            Log.e(Constants.TAG, "[COMMUNICATION THREAD] Socket is null!");
+            return;
+        }
+        try {
+            BufferedReader bufferedReader = Utilities.getReader(socket);
+            PrintWriter printWriter = Utilities.getWriter(socket);
+            if (bufferedReader == null || printWriter == null) {
+                Log.e(Constants.TAG, "[COMMUNICATION THREAD] Buffered Reader / Print Writer are null!");
+                return;
+            }
+
+            Log.i(Constants.TAG, "[COMMUNICATION THREAD] Waiting for parameters from client");
+
+            /* aici primesc stringul URL de la client */
+            String url = bufferedReader.readLine();
+//            String informationType = bufferedReader.readLine();
+            if (url == null || url.isEmpty()) {
+                Log.e(Constants.TAG, "[COMMUNICATION THREAD] Error receiving parameters from client");
+                return;
+            }
+
+            String result;
+
+            if (url.toLowerCase().contains("bad")) {
+                result = "URL Blocked by Firewall";
+            } else {
+                result = getHTML(allowedURL);
+            }
+
+            /* aici introc rezultatul la client*/
+            printWriter.println(result);
+            printWriter.flush();
+        } catch (IOException ioException) {
+            Log.e(Constants.TAG, "[COMMUNICATION THREAD] An exception has occurred: " + ioException.getMessage());
+            if (Constants.DEBUG) {
+                ioException.printStackTrace();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException ioException) {
+                    Log.e(Constants.TAG, "[COMMUNICATION THREAD] An exception has occurred: " + ioException.getMessage());
+                    if (Constants.DEBUG) {
+                        ioException.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+}
